a904c8bfecf5b2e1cdf9d7cb508df2ae60499bd6
PratikM15 <pratikmoharkar@gmail.com>
2020-07-31 23:26:35 +0530

    deploying

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Institute, Registration, Status
+from .models import Institute, Registration, Status, Contact
 
 admin.site.register(Institute)
 admin.site.register(Registration)
 admin.site.register(Status)
+admin.site.register(Contact)
