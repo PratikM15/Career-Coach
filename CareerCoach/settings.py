f10c5e7cec65daeab44533800dec7ce38882db2e
PratikM15 <pratikmoharkar@gmail.com>
2020-08-01 08:03:22 +0530

    Deploying

--- CareerCoach/settings.py
+++ CareerCoach/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's13$4zkz)hvl@#*)^9xb%(h(cd%*c=i)#f!)cbfi=y$($h5+9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
