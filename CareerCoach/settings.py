e9391b8265ebdf84164c74842bfd6d3bb5a16bc4
Pratik Ravindra Moharkar <67010811+PratikM15@users.noreply.github.com>
2020-08-01 16:09:13 +0530

    Update settings.py

--- CareerCoach/settings.py
+++ CareerCoach/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's13$4zkz)hvl@#*)^9xb%(h(cd%*c=i)#f!)cbfi=y$($h5+9%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
